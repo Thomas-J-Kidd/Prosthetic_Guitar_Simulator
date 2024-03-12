3ac624f224b95ca35a3fb80c559c74e147d83a6b
cocobyte1 <123109591+cocobyte1@users.noreply.github.com>
2024-03-12 15:44:23 -0500

    added musicxml file

--- position_mapping/processMusic.py
+++ position_mapping/processMusic.py
@@ -1,1 +1,9 @@
 # process data from music parser here
+import xml.etree.ElementTree as ET
+
+file = open("parser_output.txt", "r")
+f = file.read()
+f.type()
+print(f)
+
+tree = ET.parse('parser_output.xml')
