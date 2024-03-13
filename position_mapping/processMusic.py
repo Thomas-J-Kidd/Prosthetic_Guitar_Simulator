7c54ef4bb2d354eb052b4e76d2b591210ee56db6
cocobyte1 <123109591+cocobyte1@users.noreply.github.com>
2024-03-13 17:39:47 -0500

    now pulling note information

--- position_mapping/processMusic.py
+++ position_mapping/processMusic.py
@@ -4,31 +4,29 @@ import music_classes
 
 tree = ET.parse('Twinkle')
 score = tree.getroot()
-print(score)
 
-
-for part in score.iter('part-list'):
-    for partInfo in part.iter('score-part'):
-        songName = partInfo.find('part-name').text
-        print(songName)
-
+songName = score.find('part-list').find('score-part').find('part-name').text
 
 for part in score.iter('part'): 
-    print(part)
     for measure in part: 
         if(measure.attrib['number'] == '1'):
-            for attributes in measure.iter('attributes'):
-                divisions = attributes.find('divisions').text
-                key = attributes.get('key')
-                
-                print(key)
-
-        songInfo = measure.find('note').text
-        for note in measure.iter('note'):
-            for pitch in note.iter('pitch'):
-                name = pitch.find('step').text
-                accidental = pitch.find('alter').text
-                octave = pitch.find('octave').text
-    
+            divisions = int(measure.find('attributes').find('divisions').text)
+            keySig = int(measure.find('attributes').find('key').find('fifths').text)
+            beats = int(measure.find('attributes').find('time').find('beats').text)
+            beatType = int(measure.find('attributes').find('time').find('beat-type').text)
+            timeSig = [beats, beatType]
+            tempo = measure.find('sound').attrib['tempo']
+            print(tempo)
+        else:
+            for note in measure:
+                if note.find('pitch') is not None:
+                    name = note.find('pitch').find('step').text
+                    accidental = note.find('pitch').find('alter').text
+                    octave = note.find('pitch').find('octave').text
+                    duration = note.find('duration').text
+                elif note.find('rest') is not None:
+                    name = 'rest'
+                    duration = note.find('duration').text
+        
 
 
