c3cb0fdf5f71f05d864933db17deac52549e2f3e
cocobyte1 <123109591+cocobyte1@users.noreply.github.com>
2024-04-24 16:22:37 -0500

    fixed testing 3d back to have button

--- testing3d.py
+++ testing3d.py
@@ -4,14 +4,16 @@ position mapping module
 
 
 from position_mapping import XMLInterpret, musicClasses
+from ursina.prefabs.file_browser import FileBrowser
+from ursina.prefabs.dropdown_menu import DropdownMenu, DropdownMenuButton
 from ursina import *
 from ursina.shaders import *
 import sys
 import time
 
-
-song = XMLInterpret.XMLInterpret('musicXML_Files/output.musicxml')
-
+class dotModel(Entity): 
+    def update(self):
+        self.world_position = self.world_position
 
 def playSong(song: musicClasses.Song, counter: int):
     time.sleep(2)
@@ -32,21 +34,45 @@ guitar = Entity(model='Guitar2.obj', world_rotation=(67,158,40),color=rgb(102, 2
 guitar.enable
 
 
-dot1 = Entity(world_parent=guitar, model='circle', colorize=True, scale=(0.15,0.15,0.15), world_position=(0, 4, 3.9),
+dot1 = dotModel(world_parent=guitar, model='circle', colorize=True, scale=(0.15,0.15,0.15), world_position=(0, 4, 3.9),
             rotation=(67,158,40), collider='circle', color=rgb(255, 0, 0))
 
+filePath = 'musicXML_Files/Mary.xml'
 
-i = 0
-time.sleep(1)
-while(i < len(song.notes)):
-    space.step()
-    note = song.notes[i]    
-    length = note.noteLengthTime
-    dot1.world_position = (note.posX, note.posY, 3.9)
-    print(note.posX, note.posY)
-    time.sleep(length)
-    i = i + 1
 
+
+# demo running logic 
+def demo():
+    song = XMLInterpret.XMLInterpret(filePath)
+    i = 0
+    while(i < len(song.notes)):
+        note = song.notes[i]    
+        length = note.noteLengthTime
+        dot1.world_position = (note.posX, note.posY, 3.9)
+        dot1.update
+        print(note.posX, note.posY)
+        time.sleep(length)
+        i = i + 1
+
+
+# for choosing musicxml file to parse
+fb = FileBrowser(file_types=('.xml'), enabled=False)
+
+def on_submit(paths):
+    print('-------', paths)
+    for p in paths:
+        print('---', p)
+    filePath = paths
+fb.on_submit = on_submit
+
+def input(key):
+    if key == 'tab':
+        fb.enabled = not fb.enabled
+
+# play button
+playButton = Button(text='Play', model='circle', scale=0.15, origin=(-4.5,-2,0))
+playButton.on_click = demo
+space.run()
 
 
 
